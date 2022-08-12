1ae345299c8f014bd7edc71c82942f1e983ef1ca
Jeremy Tien <jeremy29tien@gmail.com>
2022-08-12 15:47:57 -0700

    debug

--- src/evaluating_rewards/distances/tabular.py
+++ src/evaluating_rewards/distances/tabular.py
@@ -195,7 +195,7 @@ def pearson_distance(
     vara = np.average(np.square(rewa), weights=dist)
     varb = np.average(np.square(rewb), weights=dist)
     cov = np.average(rewa * rewb, weights=dist)
-    assert cov == np.average(rewb * rewa, weights=(np.ones_like(rewb) / np.product(rewb.shape)))
+    assert cov == np.average(rewb * rewa, weights=dist)
     corr = cov / (np.sqrt(vara) * np.sqrt(varb))
     assert corr == cov / (np.sqrt(varb) * np.sqrt(vara))
     corr = min(corr, 1.0)  # floating point error sometimes rounds above 1.0
